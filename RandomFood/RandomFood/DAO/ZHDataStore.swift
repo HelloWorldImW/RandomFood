4be19b5cfc7545269ac459922ed59e9174cdbc4c
Darren <sdauwangzh@163.com>
2018-08-26 13:28:18 +0800

    [modify] datasource model

---
+++ RandomFood/RandomFood/DAO/ZHDataStore.swift
@@ -0,0 +1,13 @@
+//
+//  ZHDataStore.swift
+//  RandomFood
+//
+//  Created by DarrenW on 2018/8/26.
+//  Copyright © 2018年 Darren. All rights reserved.
+//
+
+import WCDBSwift
+
+class ZHDataStore: NSObject {
+
+}
