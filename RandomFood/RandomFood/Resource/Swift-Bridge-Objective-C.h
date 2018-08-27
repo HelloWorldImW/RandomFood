2f21970adb5e622b433219effc40a54448fd61b5
Darren <sdauwangzh@163.com>
2018-08-27 22:31:42 +0800

    [add] 添加桥接文件

---
+++ RandomFood/RandomFood/Resource/Swift-Bridge-Objective-C.h
@@ -0,0 +1,14 @@
+//
+//  Swift-Bridge-Objective-C.h
+//  RandomFood
+//
+//  Created by DarrenW on 2018/8/27.
+//  Copyright © 2018年 Darren. All rights reserved.
+//
+
+#ifndef Swift_Bridge_Objective_C_h
+#define Swift_Bridge_Objective_C_h
+
+#import "AdViewView.h"
+
+#endif /* Swift_Bridge_Objective_C_h */
--- RandomFood/RandomFood/UI/ChooseFood/ZHChooseFoodController.swift
+++ RandomFood/RandomFood/UI/ChooseFood/ZHChooseFoodController.swift
@@ -12,19 +12,6 @@ import RxSwift
 class ZHChooseFoodController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        UIApplication.shared.applicationSupportsShakeToEdit = true
-        becomeFirstResponder()
     }
     
-    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
-        print("晃动开始")
-    }
-    
-    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
-        print("晃动结束")
-    }
- 
-    override func motionCancelled(_ motion: UIEventSubtype, with event: UIEvent?) {
-        print("晃动取消")
-    }
 }
