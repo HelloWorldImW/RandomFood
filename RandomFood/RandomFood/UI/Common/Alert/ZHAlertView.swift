8f85ee2c7e3cc995672c54cf2435ab9fff626b6e
Darren <sdauwangzh@163.com>
2018-09-09 15:55:23 +0800

    add alert view

---
+++ RandomFood/RandomFood/UI/Common/Alert/ZHAlertView.swift
@@ -0,0 +1,14 @@
+//
+//  ZHAlertView.swift
+//  RandomFood
+//
+//  Created by DarrenW on 2018/9/9.
+//  Copyright © 2018年 Darren. All rights reserved.
+//
+
+import UIKit
+
+class ZHAlertView: UIView {
+
+    
+}
--- RandomFood/RandomFood/UI/Random/ZHRandomController.swift
+++ RandomFood/RandomFood/UI/Random/ZHRandomController.swift
@@ -7,15 +7,63 @@
 //
 
 import UIKit
+import RxSwift
+
+enum ZHRandomType {
+    case food
+    case diningroom
+}
 
 class ZHRandomController: ZHBaseController {
     
-    let sharkImageView = UIImageView(image: #imageLiteral(resourceName: "shark_0"))
-    let sharkTitleImageView = UIImageView(image: #imageLiteral(resourceName: "shart_title"))
+    private let sharkImageView = UIImageView(image: #imageLiteral(resourceName: "shark_0"))
+    private let sharkTitleImageView = UIImageView(image: #imageLiteral(resourceName: "shart_title"))
+    private var type: ZHRandomType?
+    
+    var diningrooms: Array<ZHDiningRoom>?
+    
+    convenience init(type randomType: ZHRandomType) {
+        self.init()
+        self.type = randomType
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        createUI()
+        diningrooms = ZHDataStore.share.searchAllDiningRooms()
+    }
+    
+    
+    
+    
+}
+
+///获取数据
+extension ZHRandomController {
+    
+}
+
+/// 摇一摇
+extension ZHRandomController {
+    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        sharkImageView.startAnimating()
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if let diningrooms = diningrooms {
+            let random = arc4random_uniform(UInt32(diningrooms.count))
+            let diningroom = diningrooms[Int(random)]
+            print(diningroom)
+        }
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+1.0) {
+            self.sharkImageView.stopAnimating()
+        }
+    }
+}
+
+/// UI相关
+extension ZHRandomController {
+    private func createUI() {
         sharkImageView.animationImages = [#imageLiteral(resourceName: "shark_0"),#imageLiteral(resourceName: "shark_1")]
         
         view.addSubview(sharkImageView)
@@ -35,17 +83,8 @@ class ZHRandomController: ZHBaseController {
             make.height.equalTo(40)
         }
         sharkImageView.animationDuration = 0.5
-        
     }
-    
-    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
-        sharkImageView.startAnimating()
-    }
-    
-    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+1.0) {
-            self.sharkImageView.stopAnimating()
-        }
-    }
-    
 }
+
+
+
